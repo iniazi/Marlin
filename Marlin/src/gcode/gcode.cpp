01842cc9062fa7cf2c61e02b5deda4d5020d34af
Scott Lahteine <github@thinkyhead.com>
2017-09-16 04:06:28 -0500

    Move M43 to cpp

--- Marlin/src/gcode/gcode.cpp
+++ Marlin/src/gcode/gcode.cpp
@@ -117,7 +117,6 @@ void GcodeSuite::dwell(millis_t time) {
 // Placeholders for non-migrated codes
 //
 extern void gcode_M18_M84();
-extern void gcode_M43();
 extern void gcode_M48();
 extern void gcode_M75();
 extern void gcode_M76();
@@ -460,7 +459,7 @@ void GcodeSuite::process_next_command() {
 
       #if ENABLED(PINS_DEBUGGING)
         case 43: // M43: Read pin state
-          gcode_M43(); break;
+          M43(); break;
       #endif
 
 
